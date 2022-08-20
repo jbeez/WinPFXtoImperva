1305c79c3add8b5ab172731b0bb6c635ce0ecbaa
Jeremiah Bright <1382636+jbeez@users.noreply.github.com>
2022-08-20 19:34:34 -0400

    Update and rename WindowsPFXtoImperva.bat to PFX_Upload_To_Imperva.bat
    add some logic to create directory if it doesn't exist, will store site_id files for future usage, can keep a collection of these for everyone
    renamed file

--- PFX_Upload_To_Imperva.bat
+++ PFX_Upload_To_Imperva.bat
@@ -1,9 +1,20 @@
-@echo off
+@echo off&setlocal enableextensions
 set RANDFILE=%~dp0\.rnd
 set /p KeyPass=Enter Key Password:
 cls
-set /p sid=Enter Numerical SiteID for your domain:
 
+mkdir sites 2> nul
+
+if exist sites/%~n1.site_id goto exists
+
+set /p sid=Enter Numerical SiteID for your domain:
+echo %sid%>sites/%~n1.site_id
+goto ssl
+
+:exists
+set /p sid=<sites/%~n1.site_id
+
+:ssl
 openssl pkcs12 -in %~nx1 -passin pass:"%KeyPass%" -passout pass:"%KeyPass%" -nocerts -out %~n1.key
 openssl pkcs12 -in %~nx1 -passin pass:"%KeyPass%" -clcerts -nokeys -out tempcert.crt
 openssl pkcs12 -in %~nx1 -passin pass:"%KeyPass%" -cacerts -nokeys -out tempcacert.crt
@@ -12,20 +23,20 @@ type tempcert.crt tempcacert.crt > %~n1.pem
 cls
 del tempcert.crt tempcacert.crt
 echo.
-echo Created %~n1.key and %~n1.pem from %~nx1
+echo Created %~n1.key and %~n1.pem
 echo.
-pause
-cls
 echo.
-echo Invoking python script to upload new certs to Imperva
+echo Invoking python script to upload %~n1.pem to Imperva
 echo.
 echo Press Enter to continue
 echo.
+pause
 cls
 echo site_id = {%sid%} > thiscert.py
 echo pemfile = "%~n1.pem" >>thiscert.py
 echo keyfile = "%~n1.key" >>thiscert.py
 echo pw = "%KeyPass%" >>thiscert.py
-python win_PFXtoImperva.py
-pause
+python PFX_Upload_To_Imperva.py
 del thiscert.py
+echo Complete
+pause
