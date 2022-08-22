d169a99ab3629d0a82d5dd77cc9eb61cf004c19d
Jeremiah Bright <1382636+jbeez@users.noreply.github.com>
2022-08-21 23:34:50 -0400

    Create PemAndKeyToImperva.py

---
+++ PemAndKeyToImperva.py
@@ -0,0 +1,72 @@
+import requests
+import base64
+import sys
+import os.path
+
+
+#Usage statement
+argumentList = sys.argv
+script = sys.argv[0]
+
+if len(sys.argv) == 1:
+        print('Usage: python3', script, ' domain site_id_number(optional, required if supplying password on command line) password(optional)')
+        print('Have the key and pem file in the same directory')
+        exit()
+
+
+#Set the domain variable from command line argument
+domain = (sys.argv[1])
+
+#Set site_id if given on the command line
+if len(sys.argv) >2:
+        site_id = int(sys.argv[2])
+        with open('sites/{}.site_id'.format(domain), 'x') as f:
+                f.write(str(site_id))
+        f.close()
+
+#Set pw if supplied on command line
+if len(sys.argv) >3:
+        pw = (sys.argv[3])
+        print(pw)
+
+#Check if pw is set, if not prompt for it
+if not 'pw' in locals():
+        pw = input('Enter Key Password: ')
+
+#Check for domain.site_id file, prompt if we don't have site_id yet
+sid = os.path.isfile('sites/{}.site_id'.format(domain))
+if not 'site_id' in locals():
+        if sid:
+                setting_data = open('sites/{}.site_id'.format(domain), 'r')
+                lines = setting_data.readlines()
+                site_id = ''
+                for i in lines:
+                  site_id = site_id + i
+        else:
+                       site_id = input('Enter Numerical SiteID: ')
+                       with open ('sites/{}.site_id'.format(domain), 'x') as f:
+                               f.write(str(site_id))
+                       f.close()
+
+
+#Convert PEM and KEY to base64 strings to upload to Imperva
+with open('{}.pem'.format(domain), "rb") as mycert:
+     b64c = base64.b64encode(mycert.read()).decode('utf-8')
+with open('{}.key'.format(domain), "rb") as mycert:
+     pk = base64.b64encode(mycert.read()).decode('utf-8')
+
+
+#Add in API ID and Key from apikey.py file
+from apikey import headers
+
+payload = {
+  "certificate": b64c,
+  "passphrase": pw,
+  "private_key": pk,
+  "auth_type": "RSA"
+}
+
+for id in site_id:
+    url = f"https://my.imperva.com/api/prov/v2/sites/{id}/customCertificate"
+    response = requests.request("PUT", url, headers=headers, json=payload)
+    print(response.text)
