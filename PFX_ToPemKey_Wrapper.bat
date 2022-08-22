79a4c4f5b3cca6f02d306ac99cad1d9200cada1a
Jeremiah Bright <1382636+jbeez@users.noreply.github.com>
2022-08-21 23:40:36 -0400

    Create PFX_ToPemKey_Wrapper.bat
    Smaller batch file, more things moved directly to python script, this just used if only have a PFX file to prep the pem and key beforehand

---
+++ PFX_ToPemKey_Wrapper.bat
@@ -0,0 +1,24 @@
+@echo off&setlocal enableextensions
+set RANDFILE=%~dp0\.rnd
+set /p KeyPass=Enter Key Password:
+cls
+
+openssl pkcs12 -in %~nx1 -passin pass:"%KeyPass%" -passout pass:"%KeyPass%" -nocerts -out %~n1.key
+openssl pkcs12 -in %~nx1 -passin pass:"%KeyPass%" -clcerts -nokeys -out tempcert.crt
+openssl pkcs12 -in %~nx1 -passin pass:"%KeyPass%" -cacerts -nokeys -out tempcacert.crt
+
+type tempcert.crt tempcacert.crt > %~n1.pem
+cls
+del tempcert.crt tempcacert.crt
+echo.
+echo Created %~n1.key and %~n1.pem
+echo.
+echo.
+echo Invoking python script to upload %~n1.pem to Imperva
+echo.
+echo Press Enter to continue
+echo.
+pause
+cls
+
+python PemAndKeyToImperva.py %~n1
